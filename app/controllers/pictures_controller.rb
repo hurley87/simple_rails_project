19ccdd5b07481e75047c3fc3902c4645d6ecc8ae
davidhurley <dhurls99@gmail.com>
2014-06-17 22:01:49 -0400

    add photo functionality added

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,17 +4,24 @@ class PicturesController < ApplicationController
 	end
 
 	def new
-		@picture = Picture.create(picture_params)
+		@picture = Picture.new
 	end
 
 	def create
-		
+		@picture = Picture.create(picture_params)
+		redirect_to root_path
+	end
+	def show
+		@picture = Picture.find(params[:id])
 	end
 
 	def delete
-
+		@picture = Picture.find(params[:id])
+		Picture.destroy(@picture)
+		redirect_to root_path
 	end
 	def update
+		@picture = Picture.find(params[:id])
 	end
 	
 	private
